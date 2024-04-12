96ceb9315e1c18042c041f8b177d9a96a04086f8
Your Name <nguyentienkhoi210@gmail.com>
2024-04-12 09:59:54 +0700

    up data, load so slow

--- books/models.py
+++ books/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.urls import reverse
 
 class Book(models.Model):
-    title  = models.CharField(max_length = 200)
-    author = models.CharField(max_length = 200)
-    description = models.CharField(max_length = 500, default=None)
+    title  = models.CharField(max_length = 500)
+    author = models.CharField(max_length = 500)
+    description = models.CharField(max_length = 2000, default=None)
     price = models.FloatField(null=True, blank=True)
     image_url = models.CharField(max_length = 2083, default=False)
     follow_author = models.CharField(max_length=2083, blank=True)  
@@ -15,7 +15,7 @@ class Book(models.Model):
 
 
 class Order(models.Model):
-	product = models.ForeignKey(Book, max_length=200, null=True, blank=True, on_delete = models.SET_NULL)
+	product = models.ForeignKey(Book, max_length=500, null=True, blank=True, on_delete = models.SET_NULL)
 	created =  models.DateTimeField(auto_now_add=True) 
 
 	def __str__(self):
