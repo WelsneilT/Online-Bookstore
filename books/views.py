18c40b657267ab68e82276bb66c3de467e367efe
vankhai <vankhai26022004@gmail.com>
2024-04-16 09:33:06 +0700

    new

---
+++ books/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render 
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin 
+from .models import Book, Order
+from django.urls import reverse_lazy
+from django.db.models import Q # for search method
+from django.http import JsonResponse
+import json
+
+
+
+class BooksListView(ListView):
+    model = Book
+    
+    def get_queryset(self):
+        # Get the queryset of all Book objects
+        queryset = super().get_queryset()
+        # Limit the queryset to the first 100 objects
+        return queryset[:100]
+    
+    template_name = 'list.html'
+
+
+class BooksDetailView(DetailView):
+    model = Book
+    template_name = 'detail.html'
+
+
+class SearchResultsListView(ListView):
+	model = Book
+
+	def get_queryset(self): # new
+		pass
+class SearchAuthorListView(SearchResultsListView):
+    template_name = 'search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(Q(author__icontains=query))
+class SearchTitleListView(SearchResultsListView):
+    template_name = 'search_title.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(Q(title__icontains=query))
+class BookCheckoutView(DetailView):
+    model = Book
+    template_name = 'checkout.html'
+    login_url     = 'login'
+
+
+def paymentComplete(request):
+	body = json.loads(request.body)
+	print('BODY:', body)
+	product = Book.objects.get(id=body['productId'])
+	Order.objects.create(
+		product=product
+	)
+	return JsonResponse('Payment completed!', safe=False)
+
