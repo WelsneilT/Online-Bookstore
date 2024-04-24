418da5282915f90e5953e72df75dfda5503d2eff
Your Name <nguyentienkhoi210@gmail.com>
2024-04-24 21:40:22 +0700

    thêm data

--- importdata.py
+++ importdata.py
@@ -9,14 +9,14 @@ dest_conn = sqlite3.connect('db.sqlite3')
 dest_cursor = dest_conn.cursor()
 
 # Retrieve data from the source database
-source_cursor.execute("SELECT title, author, description, price, coverImg FROM book_data2")
+source_cursor.execute("SELECT title, author,rating, description,language,genres,bookFormat,edition,pages,publisher,awards,likedPercent, coverImg, price  FROM book_data2")
 data_to_insert = source_cursor.fetchall()
 
 # Modify the data to include a default value for the follow_author column
-modified_data = [(title, author, description, price, coverImg,'Khoi',True) for title, author, description, price, coverImg in data_to_insert]
+modified_data = [(title, author,rating, description,language,genres,bookFormat,edition,pages,publisher,awards,likedPercent, image_url, price,True) for title, author,rating, description,language,genres,bookFormat,edition,pages,publisher,awards,likedPercent, image_url, price in data_to_insert]
 
 # Insert data into the destination database
-dest_cursor.executemany("INSERT INTO books_book (title, author, description, price, image_url, follow_author,book_available) VALUES (?, ?, ?, ?, ?, ?,?)", modified_data)
+dest_cursor.executemany("INSERT INTO books_book (title, author,rating, description,language,genres,bookFormat,edition,pages,publisher,awards,likedPercent, image_url, price,book_available) VALUES (?, ?, ?, ?, ?, ?,?,?, ?, ?, ?, ?, ?,?,?)", modified_data)
 
 # Commit changes and close connections
 dest_conn.commit()
