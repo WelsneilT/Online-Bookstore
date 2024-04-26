beed935d907ffc00103de74333c82e312a446534
WelsneilT <td.tan2711@gmail.com>
2024-04-26 10:25:11 +0700

    kết nối toàn bộ trang

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from .views import home
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', home, name='home'),
     path('books/', include('books.urls')),
+    path('index-2.html', TemplateView.as_view(template_name='index-2.html'), name='index'),
 ]
