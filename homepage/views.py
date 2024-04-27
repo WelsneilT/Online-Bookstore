fc547f23358203e7584b59337b5ace6773458909
WelsneilT <td.tan2711@gmail.com>
2024-04-27 09:51:11 +0700

    update all genres

--- homepage/views.py
+++ homepage/views.py
@@ -5,14 +5,34 @@ from django.db.models import Q
 
 @login_required
 def home(request):
-    slider_books = Book.objects.filter(book_available=True)[406:410]
+    slider_books = Book.objects.filter(book_available=True)[406:412]
     featured_products = Book.objects.filter(book_available=True)[96:116]
     new_arrivals = Book.objects.filter(book_available=True)[79:99]
     most_view_products = Book.objects.filter(book_available=True)[221:241]
-    adventure_books = Book.objects.filter(book_available=True)[295:300]
+
+    adventure_books = Book.objects.filter(
+    Q(genres__icontains='Adventure') & Q(book_available=True))[310:320]
     special_offers = Book.objects.filter(book_available=True)[500:505]
     adventure_bookss = Book.objects.filter(
-        Q(genres__icontains='Adventure') & Q(book_available=True))[510:520]
+    Q(genres__icontains='Adventure') & Q(book_available=True))[510:520]
+    biography_bookss = Book.objects.filter(
+    Q(genres__icontains='Biography') & Q(book_available=True))[10:20]
+    drama_bookss = Book.objects.filter(
+    Q(genres__icontains='Drama') & Q(book_available=True))[510:520]
+    fantasy_bookss = Book.objects.filter(
+    Q(genres__icontains='Fantasy') & Q(book_available=True))[510:520]
+    fiction_bookss = Book.objects.filter(
+    Q(genres__icontains='Fiction') & Q(book_available=True))[510:520]
+    history_bookss = Book.objects.filter(
+    Q(genres__icontains='History') & Q(book_available=True))[510:520]
+    horror_bookss = Book.objects.filter(
+    Q(genres__icontains='Horror') & Q(book_available=True))[510:520]
+    magic_bookss = Book.objects.filter(
+    Q(genres__icontains='Magic') & Q(book_available=True))[10:20]
+    non_fiction_bookss = Book.objects.filter(
+    Q(genres__icontains='Nonfiction') & Q(book_available=True))[510:520]
+    romance_bookss = Book.objects.filter(
+    Q(genres__icontains='Romance') & Q(book_available=True))[510:520]
 
     context = {
         'slider_books': slider_books, 
@@ -21,7 +41,15 @@ def home(request):
         'most_view_products': most_view_products,
         'adventure_books': adventure_books,
         'special_offers': special_offers,
-        'adventure_bookss': adventure_bookss
-    }
+        'adventure_bookss': adventure_bookss,
+        'biography_books': biography_bookss,
+        'drama_bookss' : drama_bookss,
+        'fantasy_bookss' : fantasy_bookss ,
+        'fiction_bookss' : fiction_bookss,
+        'history_bookss' : history_bookss , 
+        'horror_bookss' : horror_bookss ,
+        'magic_bookss' :    magic_bookss,
+        'non_fiction_bookss' : non_fiction_bookss,
+        'romance_bookss' :   romance_bookss ,}
 
     return render(request, 'index-2.html', context)
