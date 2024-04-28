46c1b07fc5db1aae2931be84406c5e4850ad4db5
WelsneilT <td.tan2711@gmail.com>
2024-04-29 01:31:10 +0700

    vẫn sửa

---
+++ accounts/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import SignUpView, CustomLogoutView
 from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
     path('accounts/signup/', SignUpView.as_view(), name = 'signup'),
